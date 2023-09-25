af523dce2a409064bc37d0f81b4cc5f2cfbf7579
Korolev E <nuuchcha@bk.ru>
2023-09-25 21:34:37 +0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int tiket = 16450;
+        int bonusMile = tiket / 20;
+        System.out.println("Начислено бонусов:" + bonusMile);
+    }
+}
